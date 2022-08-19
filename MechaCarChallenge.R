1ee58569b5867140d59bd31f0e4e3c87138cf0e9
Mohammadbinshahid <104873181+Mohammadbinshahid@users.noreply.github.com>
2022-08-18 23:31:59 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,25 @@ lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = 
 
 # determine p-value and r-squared value for the linear regression model
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg_df))
+
+#Deliverable 2
+# Suspension Coil Lot Analysis
+
+# read csv to dataframe
+suspension_coil_df <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(suspension_coil_df)
+
+# create a summary dataframe
+total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), Std_Dev = sd(PSI))
+View(total_summary)
+# create summaries for each lot
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), Std_Dev = sd(PSI), .groups = 'keep')
+View(lot_summary)
+
+# Deliverable 3
+# T-tests on suspension coils
+t.test(suspension_coil_df$PSI, mu=1500)
+
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
